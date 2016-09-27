6cf94e1f89e619f4c3b320016942f24d304602f2
Goutham S <gouthamsajeev1996@gmail.com>
2016-09-27 14:35:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one',function(req,res){
-  res.send("artical-one working perfect");  
+  res.send('artical-one working perfect');  
 });
 
 
